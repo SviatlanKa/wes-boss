ac6f3c619dcfffa9203cd1343e2e3339ac36c820
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-12-02 17:32:43 +0300

    finished 5 lesson

--- javascript30/05-flex-panel-image-gallery/index.js
+++ javascript30/05-flex-panel-image-gallery/index.js
@@ -1,7 +1,16 @@
-const panels = document.querySelectorAll('panel');
+const panels = document.querySelectorAll('.panel');
+console.log(panels);
 
 function handleClick() {
+    console.log("hell")
     this.classList.toggle('open');
 }
 
-panels.forEach(panel => 'click', handleClick);
+function handleTransitionEnd(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active')
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', handleClick));
+panels.forEach(panel => panel.addEventListener('transitionend', handleTransitionEnd))
